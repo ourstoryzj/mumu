488634c7df36bb61a081ed7c9924cd579d665542
ourstoryzj <ourstoryzj@163.com>
2019-12-16 15:09:47 +0800

    测试

---
+++ Operation/test1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+using excel_operation.CS;
+
+namespace excel_operation
+{
+    public class test1
+    {
+        public string ages = "18";
+
+    }
+}
