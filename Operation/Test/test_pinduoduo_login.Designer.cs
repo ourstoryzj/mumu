4ff4247e21c5b64081f5739de1e1ffbce66c9e8b
dingyu <ourstoryzj@163.com>
2019-12-26 20:43:40 +0800

    1

--- Operation/Test/test_pinduoduo_login.Designer.cs
+++ Operation/Test/test_pinduoduo_login.Designer.cs
@@ -28,93 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txt_url = new System.Windows.Forms.TextBox();
-            this.txt_html = new System.Windows.Forms.TextBox();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.button1 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.tabControl1.SuspendLayout();
-            this.tabPage1.SuspendLayout();
-            this.tabPage2.SuspendLayout();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // txt_url
+            // panel1
             // 
-            this.txt_url.Location = new System.Drawing.Point(22, 52);
-            this.txt_url.Name = "txt_url";
-            this.txt_url.Size = new System.Drawing.Size(441, 21);
-            this.txt_url.TabIndex = 1;
-            this.txt_url.TextChanged += new System.EventHandler(this.txt_url_TextChanged);
-            // 
-            // txt_html
-            // 
-            this.txt_html.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.txt_html.Location = new System.Drawing.Point(3, 98);
-            this.txt_html.Multiline = true;
-            this.txt_html.Name = "txt_html";
-            this.txt_html.Size = new System.Drawing.Size(820, 464);
-            this.txt_html.TabIndex = 1;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(22, 23);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(228, 23);
-            this.button3.TabIndex = 0;
-            this.button3.Text = "第三种方法(优秀:速度快,解析全面)";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(528, 60);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(102, 23);
-            this.button4.TabIndex = 0;
-            this.button4.Text = "模拟登陆";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
-            // 
-            // tabControl1
-            // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(12, 56);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(862, 612);
-            this.tabControl1.TabIndex = 2;
-            // 
-            // tabPage1
-            // 
-            this.tabPage1.Controls.Add(this.panel1);
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(854, 586);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
-            this.tabPage1.UseVisualStyleBackColor = true;
-            // 
-            // tabPage2
-            // 
-            this.tabPage2.Controls.Add(this.txt_html);
-            this.tabPage2.Controls.Add(this.txt_url);
-            this.tabPage2.Controls.Add(this.button4);
-            this.tabPage2.Controls.Add(this.button3);
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(854, 586);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel1.Location = new System.Drawing.Point(423, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(451, 656);
+            this.panel1.TabIndex = 0;
             // 
             // button1
             // 
@@ -126,38 +51,20 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
-            // panel1
-            // 
-            this.panel1.Location = new System.Drawing.Point(32, 6);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(315, 574);
-            this.panel1.TabIndex = 0;
-            // 
             // test_pinduoduo_login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(886, 680);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.tabControl1);
             this.Name = "test_pinduoduo_login";
             this.Text = "test_meau";
-            this.tabControl1.ResumeLayout(false);
-            this.tabPage1.ResumeLayout(false);
-            this.tabPage2.ResumeLayout(false);
-            this.tabPage2.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-        private System.Windows.Forms.TextBox txt_url;
-        private System.Windows.Forms.TextBox txt_html;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Panel panel1;
     }
--- Operation/Test/test_pinduoduo_login.cs
+++ Operation/Test/test_pinduoduo_login.cs
@@ -24,14 +24,14 @@ namespace excel_operation.Test
 
             webBrowser1 = new ChromiumWebBrowser("https://mobile.yangkeduo.com/personal.html?refer_page_name=index&refer_page_id=10002_1574496288865_ZKZKA8HgWJ&refer_page_sn=10002&page_id=10001_1577246196934_4h0wdL6yBD&is_back=1");
 
-            //try
-            //{
-            //    webBrowser1.RequestHandler = new MyRequestHandler();
-            //}
-            //catch (Exception ex)
-            //{
-            //    ex.ToString().ToShow();
-            //}
+            try
+            {
+                webBrowser1.RequestHandler = new MyRequestHandler();
+            }
+            catch (Exception ex)
+            {
+                ex.ToString().ToShow();
+            }
             // webBrowser1.KeyboardHandler = new CefKeyboardHandler();
             //var setting = new CefSharp.CefSettings();
             //setting.UserAgent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36";
@@ -130,153 +130,7 @@ namespace excel_operation.Test
                 bool b = task.Result;
             }
         }
-
-
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            //string path1 = Application.StartupPath + "\\1.zip";
-            //string path2 = Application.StartupPath+"\\1" ;
-            ////Common.RarHelper.ExeRAR(path1, "AutoUpdater.zip");
-            ////Common.RarHelper.ExeRAR2(path1, path2);
-            ////Common.RarHelper.CondenseRarOrZip(path1, path1 + ".rar", true, "");
-            //MessageBox.Show("成功");
-            //if (NetHelper.IsInternetAvailable())
-            //    if (Common.WiFiHelper.HasNetWork())
-
-            //    "有网络".ToShow();
-
-            //else
-            //    "无网络".ToShow();
-            ToWebClient();
-        }
-
-
-        void ToWebClient()
-        {
-            try
-            {
-                WebClient MyWebClient = new WebClient();
-
-                MyWebClient.Credentials = CredentialCache.DefaultCredentials;//获取或设置用于向Internet资源的请求进行身份验证的网络凭据
-                Byte[] pageData = MyWebClient.DownloadData(txt_url.Text); //从指定网站下载数据
-                string pageHtml = Encoding.Default.GetString(pageData);  //如果获取网站页面采用的是GB2312，则使用这句            
-                                                                         //string pageHtml = Encoding.UTF8.GetString(pageData); //如果获取网站页面采用的是UTF-8，则使用这句
-                Console.WriteLine(pageHtml);//在控制台输入获取的内容
-                //using (StreamWriter sw = new StreamWriter("c:\\test\\ouput.html"))//将获取的内容写入文本
-                //{
-                //    sw.Write(pageHtml);
-                //}
-                txt_html.Text = pageHtml;
-
-                Console.ReadLine(); //让控制台暂停,否则一闪而过了             
-            }
-            catch (WebException webEx)
-            {
-                Console.WriteLine(webEx.Message.ToString());
-            }
-            "成功".ToShow();
-        }
-
-
-        void ToWebBrowser()
-        {
-            WebBrowser web = new WebBrowser();
-            web.Navigate(txt_url.Text);
-            web.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(web_DocumentCompleted);
-            "成功".ToShow();
-        }
-        void web_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
-        {
-            WebBrowser web = (WebBrowser)sender;
-            HtmlElementCollection ElementCollection = web.Document.GetElementsByTagName("Body");
-
-            foreach (HtmlElement item in ElementCollection)
-            {
-                //File.AppendAllText("Kaijiang_xj.txt", item.InnerText);
-                txt_html.Text = txt_html.Text + item.InnerText;
-            }
-        }
-
-
-
-        HttpWebRequest httpReq;
-        HttpWebResponse httpResp;
-
-        string strBuff = "";
-        char[] cbuffer = new char[256];
-        int byteRead = 0;
-
-        //string filename = @"c:\log.txt";
-        ///定义写入流操作 
-        public void ToWriteStream()
-        {
-            Uri httpURL = new Uri(txt_url.Text);
-            ///HttpWebRequest类继承于WebRequest，并没有自己的构造函数，需通过WebRequest的Creat方法 建立，并进行强制的类型转换 
-            httpReq = (HttpWebRequest)WebRequest.Create(httpURL);
-            ///通过HttpWebRequest的GetResponse()方法建立HttpWebResponse,强制类型转换
-            httpResp = (HttpWebResponse)httpReq.GetResponse();
-            ///GetResponseStream()方法获取HTTP响应的数据流,并尝试取得URL中所指定的网页内容
-            ///若成功取得网页的内容，则以System.IO.Stream形式返回，若失败则产生ProtoclViolationException错 误。在此正确的做法应将以下的代码放到一个try块中处理。这里简单处理 
-            Stream respStream = httpResp.GetResponseStream();
-            ///返回的内容是Stream形式的，所以可以利用StreamReader类获取GetResponseStream的内容，并以StreamReader类的Read方法依次读取网页源程序代码每一行的内容，直至行尾（读取的编码格式：UTF8） 
-            StreamReader respStreamReader = new StreamReader(respStream, Encoding.UTF8);
-            //byteRead = respStreamReader.Read(cbuffer, 0, 256);
-
-            //while (byteRead != 0)
-            //{
-            //    string strResp = new string(cbuffer, 0, byteRead);
-            //    strBuff = strBuff + strResp;
-            //    byteRead = respStreamReader.Read(cbuffer, 0, 256);
-            //}
-            txt_html.Text = respStreamReader.ReadToEnd();
-            respStream.Close();
-            //txt_html.Text = strBuff;
-            "成功".ToShow();
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            ToWebBrowser();
-
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            ToWriteStream();
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            string strId = "guest";
-            string strPassword = "123456";
-
-            ASCIIEncoding encoding = new ASCIIEncoding();
-            string postData = "userid=" + strId;
-            postData += ("&password=" + strPassword);
-
-            byte[] data = encoding.GetBytes(postData);
-
-            // Prepare web request
-            HttpWebRequest myRequest =
-            (HttpWebRequest)WebRequest.Create("http://www.sina.com/login.asp");
-
-            myRequest.Method = "POST";
-            myRequest.ContentType = "application/x-www-form-urlencoded";
-            myRequest.ContentLength = data.Length;
-            Stream newStream = myRequest.GetRequestStream();
-
-            // Send the data.
-            newStream.Write(data, 0, data.Length);
-            newStream.Close();
-
-            // Get response
-            //HttpWebResponse myResponse=(HttpWebResponse)myRequest.GetResponse();
-            //StreamReader reader = new StreamReader(response.GetResponseStream(),Encoding.Default);
-            //string content = reader.ReadToEnd();
-            //Response.Write(content); 
-        }
-
+ 
         private void txt_url_TextChanged(object sender, EventArgs e)
         {
 
